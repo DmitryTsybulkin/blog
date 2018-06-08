ad36d24f39f608a9540762968a8c06ef40a7f567
dmitriy.tsybulkin <dmitriy.tsybulkin@corp.codeinside.ru>
2018-06-08 17:41:48 +0300

    t8 rebase from master

--- src/test/java/com/pany/blog/model/PostTests.java
+++ src/test/java/com/pany/blog/model/PostTests.java
@@ -27,103 +27,103 @@ import static org.junit.Assert.*;
 public class PostTests {
 
 
-	@Autowired
-	private UserRep userRep;
+    @Autowired
+    private UserRep userRep;
 
-	@Autowired
-	private PostRep postRep;
+    @Autowired
+    private PostRep postRep;
 
-	@Autowired
-	private RoleRep roleRep;
+    @Autowired
+    private RoleRep roleRep;
 
-	@Autowired
-	private CommentRep commentRep;
+    @Autowired
+    private CommentRep commentRep;
 
-	private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles = new HashSet<>();
 
-	private List<Comment> comments = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
 
-	private List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
 
-	private User user = new User();
+    private User user = new User();
 
-	private Post post;
+    private Post post;
 
-	@Before
-	public void initDb() throws Exception {
-		roles.add(roleRep.save(new Role("ADMIN")));
-		user = userRep.save(new User("login", "password", roles, null));
-		post = postRep.save(new PostBuilder()
-				.withHeader("header")
-				.withContent("content")
-				.withUser(user)
-				.withComments(null)
-				.withDateCreated(LocalDateTime.now())
-				.build());
-		posts.add(post);
-		user.setPosts(posts);
-		comments.add(new Comment(user.getLogin(), "text", LocalDateTime.now(), post));
-		comments.add(new Comment(user.getLogin(), "one_more_text", LocalDateTime.now(), post));
-	}
+    @Before
+    public void initDb() throws Exception {
+        roles.add(roleRep.save(new Role("ADMIN")));
+        user = userRep.save(new User("login", "password", roles, null));
+        post = postRep.save(new PostBuilder()
+                .withHeader("header")
+                .withContent("content")
+                .withUser(user)
+                .withComments(null)
+                .withDateCreated(LocalDateTime.now())
+                .build());
+        posts.add(post);
+        user.setPosts(posts);
+        comments.add(new Comment(user.getLogin(), "text", LocalDateTime.now(), post));
+        comments.add(new Comment(user.getLogin(), "one_more_text", LocalDateTime.now(), post));
+    }
 
-	@Test
-	public void createPost() throws Exception {
-		Post best_post = postRep.save(new PostBuilder()
-				.withHeader("head")
-				.withContent("cont")
-				.withComments(null)
-				.withDateCreated(LocalDateTime.now())
-				.withUser(user)
-				.build());
-		Optional<Post> targetPost = Optional.of(postRep.findPostByHeader(best_post.getHeader()).orElseThrow(ResourceNotFoundException::new));
-		assertEquals(targetPost.get().getContent(), best_post.getContent());
-		assertEquals(targetPost.get().getUser().getLogin(), best_post.getUser().getLogin());
-		assertEquals(targetPost.get().getUser().getId(), best_post.getUser().getId());
-		assertEquals(targetPost.get().getId(), best_post.getId());
-		assertEquals(targetPost.get().getDateCreated(), best_post.getDateCreated());
-		assertNull(targetPost.get().getComments());
-	}
+    @Test
+    public void createPost() throws Exception {
+        Post best_post = postRep.save(new PostBuilder()
+                .withHeader("head")
+                .withContent("cont")
+                .withComments(null)
+                .withDateCreated(LocalDateTime.now())
+                .withUser(user)
+                .build());
+        Optional<Post> targetPost = Optional.of(postRep.findPostByHeader(best_post.getHeader()).orElseThrow(ResourceNotFoundException::new));
+        assertEquals(targetPost.get().getContent(), best_post.getContent());
+        assertEquals(targetPost.get().getUser().getLogin(), best_post.getUser().getLogin());
+        assertEquals(targetPost.get().getUser().getId(), best_post.getUser().getId());
+        assertEquals(targetPost.get().getId(), best_post.getId());
+        assertEquals(targetPost.get().getDateCreated(), best_post.getDateCreated());
+        assertNull(targetPost.get().getComments());
+    }
 
-	@Test
-	public void listPosts() throws Exception {
-		List<Post> postes = postRep.findAll();
-		assertFalse(postes.isEmpty());
-		assertTrue(postes.containsAll(posts));
-		Post best_post = postes.get(0);
-		Post post = posts.get(0);
-		assertEquals(best_post.getId(), post.getId());
-		assertEquals(best_post.getUser().getId(), post.getUser().getId());
-		assertEquals(best_post.getUser().getLogin(), post.getUser().getLogin());
-		assertEquals(best_post.getDateCreated(), post.getDateCreated());
-		assertEquals(best_post.getHeader(), post.getHeader());
-		assertEquals(best_post.getContent(), post.getContent());
-	}
+    @Test
+    public void listPosts() throws Exception {
+        List<Post> postes = postRep.findAll();
+        assertFalse(postes.isEmpty());
+        assertTrue(postes.containsAll(posts));
+        Post best_post = postes.get(0);
+        Post post = posts.get(0);
+        assertEquals(best_post.getId(), post.getId());
+        assertEquals(best_post.getUser().getId(), post.getUser().getId());
+        assertEquals(best_post.getUser().getLogin(), post.getUser().getLogin());
+        assertEquals(best_post.getDateCreated(), post.getDateCreated());
+        assertEquals(best_post.getHeader(), post.getHeader());
+        assertEquals(best_post.getContent(), post.getContent());
+    }
 
-	@Test
-	public void updatePost() throws Exception {
-		Post post = posts.get(0);
-		post.setHeader("newHeader");
-		post.setContent("contt");
-		Post targetPost = postRep.getOne(post.getId());
-		assertEquals(targetPost.getHeader(), "newHeader");
-		assertEquals(targetPost.getContent(), "contt");
-		assertEquals(targetPost.getDateCreated(), post.getDateCreated());
-		assertEquals(targetPost.getUser().getId(), post.getUser().getId());
-		assertEquals(targetPost.getUser().getLogin(), post.getUser().getLogin());
-	}
+    @Test
+    public void updatePost() throws Exception {
+        Post post = posts.get(0);
+        post.setHeader("newHeader");
+        post.setContent("contt");
+        Post targetPost = postRep.getOne(post.getId());
+        assertEquals(targetPost.getHeader(), "newHeader");
+        assertEquals(targetPost.getContent(), "contt");
+        assertEquals(targetPost.getDateCreated(), post.getDateCreated());
+        assertEquals(targetPost.getUser().getId(), post.getUser().getId());
+        assertEquals(targetPost.getUser().getLogin(), post.getUser().getLogin());
+    }
 
-	@Test
-	public void deletePost() throws Exception {
-		Long id = post.getId();
-		user.setPosts(null);
-		postRep.deleteById(id);
-		assertFalse(postRep.existsById(id));
-	}
+    @Test
+    public void deletePost() throws Exception {
+        Long id = post.getId();
+        user.setPosts(null);
+        postRep.deleteById(id);
+        assertFalse(postRep.existsById(id));
+    }
 
-	public void dropDb() throws Exception {
-		postRep.deleteAllInBatch();
-		commentRep.deleteAllInBatch();
-		userRep.deleteAllInBatch();
-	}
+    public void dropDb() throws Exception {
+        postRep.deleteAllInBatch();
+        commentRep.deleteAllInBatch();
+        userRep.deleteAllInBatch();
+    }
 
 }
